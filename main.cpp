f4f961378a75b5b63a278e6695249cdf1c1c2d44
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2024-09-26 20:49:53 +0300

    menu created, case 1 WIP

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
 #include <stdio.h>;
+#include <conio.h>;
+#include <stdlib.h>;
+//Программа расчета калорийности продуктов (вес*калорийность и 
+//сложение ингредиентов). 
+
 void main(){
- 
+    short int selector = -1;
+    Eat* array = (Eat*)malloc(sizeof(Eat)*100);
+    int counter = 0;
+    while (selector){
+        printf("1 - Добавить продукт\n2 - Рассчитать итоговую калорийность\n0 - Выход\n");
+        scanf_s("%d",&selector);
+        switch(selector){
+            case 1:{
+                printf("Введите вес: ");
+                scanf_s("%d",*(array[i].weight));
+                printf("Введите калорийность на 100 грамм: ");
+                scanf_s("%d",*(array[i].callories));
+            }
+            case 2:{}
+            case 0:{selector =0;break}
+            default:{printf("Неверный номер действия, повторите попытку\n")}
+        }
+    }
 }
